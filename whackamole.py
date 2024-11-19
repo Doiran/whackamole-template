ffe06416a3dbdb0c18648883a5970f663d98be70
Doiran <grim_gravewalker@protonmail.com>
2024-11-18 19:53:45 -0500

    added the whack a mole gameplay loop

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -10,20 +10,24 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+        mole_x = 0
+        mole_y = 0
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x,y = event.pos
+                    if mole_x <= x < mole_x + 32 and mole_y <= y < mole_y + 32:
+                        mole_x = random.randrange(0,20) * 32
+                        mole_y = random.randrange(0, 16) *32
+
             screen.fill("light blue")
-            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
-            x = 0
-            for i in range(32):
-                x += 32
-                pygame.draw.line(screen, 'black', start_pos=(x, 0), end_pos=(x,512))
-            y = 0
-            for j in range(32):
-                y+= 32
-                pygame.draw.line(screen, 'black', start_pos=(0, y), end_pos=(640, y))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_x, mole_y)))
+            for i in range(1,20):
+                pygame.draw.line(screen, 'black', (i * 32, 0), (i* 32,640))
+            for j in range(1,16):
+                pygame.draw.line(screen, 'black', (0, j*32), (640, j*32))
             pygame.display.flip()
             clock.tick(60)
     finally:
